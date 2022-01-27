a3d7243403b07101c357b6f14e03c4368c5639ae
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2022-01-27 12:45:33 +0100

    Mise en place d'un script d'automatisation

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+echo "Creation de l'Application a déployer"
+mkdir ./Application
+mkdir ./Application/www
+
+cp -R backend ./Application/www
+
+
+cp -R frontend/build ./Application/www/backend
+
+rm -rf ./Application/www/backend/node_modules 
+rm -rf ./Application/www/backend/.env 
+
+
+cp docker/docker-compose.yml ./Application/
+
+echo 'Success'
