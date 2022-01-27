f55ae17c18f84a067fe3c366daf1b719a2b82320
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2022-01-27 17:00:03 +0100

    Modification du fichier script automatisation

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,11 +5,9 @@ mkdir ./Application/www
 
 cp -R backend ./Application/www
 
-
 cp -R frontend/build ./Application/www/backend
 
 rm -rf ./Application/www/backend/node_modules 
-
 
 cp docker/docker-compose.yml ./Application/
 
