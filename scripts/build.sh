e6b48386a2501d0c9c9bfeea8b45016cd126880f
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2022-02-12 20:42:52 +0100

    Modification script pour deploiement avec DB

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,13 +2,15 @@ echo "Creation de l'Application a déployer"
 cd ..
 mkdir ./Application
 mkdir ./Application/www
-
+mkdir ./Application/database
 cp -R backend ./Application/www
 
 cp -R frontend/build ./Application/www/backend
 
 rm -rf ./Application/www/backend/node_modules 
+rm -rf ./Application/www/backend/.env 
 
 cp docker/docker-compose.yml ./Application/
+
 
 echo 'Success'
