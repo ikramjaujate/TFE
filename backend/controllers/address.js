58b1cdc03a4044906ca614daf56a71cc18ea1306
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2022-02-13 13:05:38 +0100

    Création de API documentation avec Swagger + API pour users et adress

--- backend/controllers/address.js
+++ backend/controllers/address.js
@@ -17,8 +17,7 @@ const getAllAddress = async (req, res) => {
         return res.status(200).json({ address });
     } catch (error) {
         return res.status(500).send(error.message);
-    }
-    
+    } 
 }
 module.exports = {
     createAddress,
