d72138006a1b9640f5ad6f3ee95ab6f4b60cb446
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2022-02-19 15:21:35 +0100

    Création du navbar pour projet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import styled from 'styled-components';
-import Sidebar from './components/Sidebar'
-import Dashboard from './components/Dashboard'
-
-import {Router, Route} from 'react-router-dom'
+import './App.css';
+import Navbar from './components/Navbar';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route}  from 'react-router-dom'
+import Home from './pages/Home'
+import Clientes from './pages/Clientes'
+import Vehiculos from './pages/Vehiculos'
+import Remisiones from './pages/Remisiones'
+import Facturas from './pages/Facturas'
+import Historiales from './pages/Historiales'
 function App() {
   return (
-    <Div>
-        <Sidebar />
-        <Dashboard />
-
-    </Div>
-
+    <>
+    <Router>
+      <Navbar/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/clients" component={Clientes}/>
+          <Route path="/projects" component={Vehiculos}/>
+          <Route path="/invoices" component={Remisiones}/>
+          <Route path="/employees" component={Facturas}/>
+          <Route path="/material" component={Historiales}/>
+        </Switch>
+    </Router>
+    </>
   );
 }
 
 export default App;
-const Div = styled.div `
-position: relative;
-`;
