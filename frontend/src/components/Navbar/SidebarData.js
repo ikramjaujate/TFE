0f27f931a7099ca29cbc4e1a1ac70763e93cb862
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2022-02-19 18:19:12 +0100

    Correction couleur navbar

--- frontend/src/components/Navbar/SidebarData.js
+++ frontend/src/components/Navbar/SidebarData.js
@@ -7,37 +7,37 @@ export const SidebarData = [
     {
         title: 'Home',
         path: '/',
-        icon: <RiIcons.RiLayoutGridLine/>,
+        icon: <RiIcons.RiLayoutGridLine style={{color: 'white'}}/>,
         cName: 'nav-text'
     },
     {
         title: 'Clients',
         path: '/clients',
-        icon: <AiIcons.AiOutlineUser/>,
+        icon: <AiIcons.AiOutlineUser style={{color: 'white'}}/>,
         cName: 'nav-text'
     },
     {
         title: 'Projects',
         path: '/projects',
-        icon: <RiIcons.RiBook2Fill/>,
+        icon: <RiIcons.RiBook2Fill style={{color: 'white'}}/>,
         cName: 'nav-text'
     },
     {
         title: 'Invoices',
         path: '/invoices',
-        icon: <RiIcons.RiDraftLine/>,
+        icon: <RiIcons.RiDraftLine style={{color: 'white'}}/>,
         cName: 'nav-text'
     },
     {
         title: 'Employees',
         path: '/employees',
-        icon: <AiIcons.AiOutlineUsergroupAdd/>,
+        icon: <AiIcons.AiOutlineUsergroupAdd style={{color: 'white'}}/>,
         cName: 'nav-text'
     },
     {
         title: 'Material',
         path: '/material',
-        icon: <RiIcons.RiShoppingBasket2Line/>,
+        icon: <RiIcons.RiShoppingBasket2Line style={{color: 'white'}}/>,
         cName: 'nav-text'
     },
 ]
