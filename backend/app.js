76a8610e5266855494a2bb2995eb1b0419656df9
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2022-02-20 13:25:12 +0100

    Header CSP error

--- backend/app.js
+++ backend/app.js
@@ -55,10 +55,10 @@ app.use(
 );
 
 // CSP Header middlewar 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   res.setHeader("content-security-policy-report-only", "default-src 'self'; font-src 'self' data: https://maxcdn.bootstrapcdn.com; img-src 'self' http://c0nrad.io; object-src 'none'; script-src 'report-sample' 'self'; style-src 'report-sample' 'self' https://maxcdn.bootstrapcdn.com; base-uri 'none'; report-uri https://5e52f4c893efdeaaa7d40460.endpoint.csper.io;"),
   next();
- });
+ });*/
 
 //Expect-CT
 app.use(expectCt({ maxAge: 86400 }));
