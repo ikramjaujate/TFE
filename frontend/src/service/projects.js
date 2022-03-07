2d16985d9d948da6df3f1866913ae188f1792bd7
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2022-03-07 16:16:12 +0100

    Route pour obtenir tout les projets

---
+++ frontend/src/service/projects.js
@@ -0,0 +1,19 @@
+const GetProjects = async () => {
+
+    let informations = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer " + localStorage.getItem('access_token')
+      },
+    };
+    return await (fetch(`/api/projects`, informations).then(response => {
+        console.log(response)
+      return response.json()
+    }))
+  
+  }
+
+  export {
+    GetProjects
+  }
