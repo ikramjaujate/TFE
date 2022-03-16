0411bc5201e0b16459eb72a4c5729d63d00de4fe
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2022-03-16 15:28:51 +0100

    Update model documents

--- backend/models/document.js
+++ backend/models/document.js
@@ -4,12 +4,12 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   const Document = sequelize.define('Document', {
-    idDocument : {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true 
-      },
-    file : {
+    idDocument: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    file: {
       type: DataTypes.BLOB,
       allowNull: false
     },
@@ -20,9 +20,23 @@ module.exports = (sequelize, DataTypes) => {
     type: {
       type: DataTypes.ENUM('devis', 'facture'),
       allowNull: false
-  }
+    },
+    isAccepted: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true
+    },
+    idPaid: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true
+    },
+    notes: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+
+
   }, {});
-  
+
   Document.associate = function (models) {
     // associations can be defined here
     Document.belongsTo(models.Project, {
