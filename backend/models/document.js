980d9ea59058b38b0770475018b4e09191c599a9
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2022-03-20 12:27:38 +0100

    Rajout de section isEmalied dans model pour DB

--- backend/models/document.js
+++ backend/models/document.js
@@ -36,6 +36,11 @@ module.exports = (sequelize, DataTypes) => {
     notes: {
       type: DataTypes.STRING,
       allowNull: true
+    },
+    isEmailed: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
     }
 
 
