ca90b1bcedf406827ea1715a2ef4c7edacc3b745
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2022-04-03 15:58:28 +0200

    Correction des seeders avec suppresion de idMaterial

--- backend/seeders/7-material.js
+++ backend/seeders/7-material.js
@@ -2,7 +2,7 @@ module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('Material', [
             {
-                idMaterial: 1,
+
                 name: "Brick Pavers",
                 quantity: 3,
                 price: 4.51,
@@ -12,7 +12,7 @@ module.exports = {
                 updatedAt: new Date()
             },
             {
-                idMaterial: 2,
+
                 name: "Doors, Frames & Hardware",
                 quantity: 18,
                 price: 168.85,
@@ -22,7 +22,7 @@ module.exports = {
                 updatedAt: new Date()
             },
             {
-                idMaterial: 3,
+
                 name: "Drywall & Acoustical (FED)",
                 quantity: 12,
                 price: 14,
@@ -32,7 +32,7 @@ module.exports = {
                 updatedAt: new Date()
             },
             {
-                idMaterial: 4,
+
                 name: "Wall Protection",
                 quantity: 4,
                 price: 3,
@@ -42,7 +42,7 @@ module.exports = {
                 updatedAt: new Date()
             },
             {
-                idMaterial: 5,
+
                 name: "Soft Flooring and Base",
                 quantity: 9,
                 price: 10.5,
@@ -52,7 +52,7 @@ module.exports = {
                 updatedAt: new Date()
             },
             {
-                idMaterial: 6,
+
                 name: "Overhead Doors",
                 quantity: 2,
                 price: 500,
@@ -62,7 +62,7 @@ module.exports = {
                 updatedAt: new Date()
             },
             {
-                idMaterial: 7,
+
                 name: "Roofing (Metal)",
                 quantity: 14,
                 price: 21.84,
@@ -72,7 +72,7 @@ module.exports = {
                 updatedAt: new Date()
             },
             {
-                idMaterial: 8,
+
                 name: "Retaining Wall",
                 quantity: 3,
                 price: 23.51,
@@ -82,7 +82,7 @@ module.exports = {
                 updatedAt: new Date()
             },
             {
-                idMaterial: 9,
+
                 name: "Glass & Glazing",
                 quantity: 4,
                 price: 41.25,
