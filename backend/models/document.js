38b3523f2df27d817f3734ed0ac03ac232d57c5c
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2022-04-17 11:04:45 +0200

    Correction nom de champ pour les documents

--- backend/models/document.js
+++ backend/models/document.js
@@ -29,7 +29,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.BOOLEAN,
       allowNull: true
     },
-    idPaid: {
+    isPaid: {
       type: DataTypes.BOOLEAN,
       allowNull: true
     },
